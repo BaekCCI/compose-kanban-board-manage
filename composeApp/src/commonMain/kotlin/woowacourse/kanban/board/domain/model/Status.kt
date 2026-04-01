2acbb9f4cb5a14e0f67610cf2b3c9f1bec06c24b
baekcii <bsy8470@naver.com>
2026-04-01 17:00:18 +0900

    feat: 상태별 특징 및 전이 규칙 정의

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/model/Status.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/model/Status.kt
@@ -1,7 +1,29 @@
 package woowacourse.kanban.board.domain.model
 
-enum class Status {
-    TODO,
-    IN_PROGRESS,
-    DONE,
+enum class Status(val requiredAssignee: Boolean, val canDeleteTask: Boolean) {
+    TODO(false, true),
+    IN_PROGRESS(true, true),
+    REVIEW(true, false),
+    DONE(true, false),
+    ;
+
+    val movableTo: List<Status>
+        get() = when (this) {
+            TODO -> listOf(TODO, IN_PROGRESS)
+            IN_PROGRESS -> listOf(TODO, REVIEW)
+            REVIEW -> listOf(IN_PROGRESS, DONE)
+            DONE -> listOf(TODO)
+        }
+
+    companion object {
+        fun canChangeStatus(from: Status, to: Status, hasAssignee: Boolean): Boolean {
+            return when (from) {
+                TODO -> {
+                    if (!hasAssignee) false else to in from.movableTo
+                }
+
+                else -> to in from.movableTo
+            }
+        }
+    }
 }
