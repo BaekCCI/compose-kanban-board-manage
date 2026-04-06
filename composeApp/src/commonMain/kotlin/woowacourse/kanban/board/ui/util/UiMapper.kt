c3969632278c8164a0298a20e7fe13972cd91f9e
baekcii <bsy8470@naver.com>
2026-04-06 15:23:57 +0900

    feat: 스낵바 메시지 리소스 추가 및 에러 매핑 구현

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/util/UiMapper.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/util/UiMapper.kt
@@ -6,11 +6,17 @@ import kanbanboard.composeapp.generated.resources.error_tag_count_exceeded
 import kanbanboard.composeapp.generated.resources.error_tag_invalid_format
 import kanbanboard.composeapp.generated.resources.error_tag_invalid_length
 import kanbanboard.composeapp.generated.resources.error_title_blank
+import kanbanboard.composeapp.generated.resources.snackbar_error_create_new_task
+import kanbanboard.composeapp.generated.resources.snackbar_error_delete_task
+import kanbanboard.composeapp.generated.resources.snackbar_error_invalid_status_transition
+import kanbanboard.composeapp.generated.resources.snackbar_project_not_found
+import kanbanboard.composeapp.generated.resources.snackbar_task_not_found
 import kanbanboard.composeapp.generated.resources.status_done
 import kanbanboard.composeapp.generated.resources.status_in_progress
 import kanbanboard.composeapp.generated.resources.status_review
 import kanbanboard.composeapp.generated.resources.status_todo
 import org.jetbrains.compose.resources.StringResource
+import woowacourse.kanban.board.domain.model.KanbanError
 import woowacourse.kanban.board.domain.model.Status
 import woowacourse.kanban.board.domain.validator.ValidationError
 
@@ -27,4 +33,13 @@ fun Status.toUiString(): StringResource = when (this) {
     Status.IN_PROGRESS -> Res.string.status_in_progress
     Status.REVIEW -> Res.string.status_review
     Status.DONE -> Res.string.status_done
+}
+
+fun KanbanError.toMessage(): StringResource = when (this) {
+    is KanbanError.TaskCreationFailed -> Res.string.snackbar_error_create_new_task
+    is KanbanError.ProjectNotFound -> Res.string.snackbar_project_not_found
+    is KanbanError.TaskNotFound -> Res.string.snackbar_task_not_found
+    is KanbanError.CannotDeleteTask -> Res.string.snackbar_error_delete_task
+    is KanbanError.InvalidStatusTransition -> Res.string.snackbar_error_invalid_status_transition
+    is KanbanError.AssigneeRequired -> Res.string.error_required_assignee
 }
